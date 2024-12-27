dcf741f2a72f8ad2b5b3b676283b88a8a0d02bd0
shrutisahu16 <sahushruti510@gmail.com>
2024-12-27 23:35:22 +0530

    add

--- l5/app.js
+++ l5/app.js
@@ -1,2 +1,42 @@
 //event -> Something happened is called event 
 //Event Listener -> event ko listen krke response ko return krne ka kaam event listener la hota h yani event ne kya kaam kiya h uska response event listner return krta hai
+
+
+// const Emitter = require("./emitter");-->custom event
+const Emitter = require("events"); //event core nodejs module by default present in nodejs
+const events = require("./config").events;
+// console.log(events); //Output events: { GREET: 'greet', FILESAVED: 'filesaved', FILEOPEN: 'fileopen' }
+
+//bad practice--kuki greet bar bar likha h agr hume change krna hoga to hume har jhga change krna hoga jha jha greet h to isse bachne ke liye hum ek naya module create krenge jisse apne app changes reflect ho jaye sab jhga ek jhga change krne par
+const emitr = new Emitter();//object created
+// console.log(emitr); // Output:--Emitter { events: {} }
+//on is used to add a callback function
+// emitr.on("greet",function(){
+//     console.log("Hello");
+// });
+
+// emitr.on("greet",function(){
+//     console.log("greet occured!");
+// });//added 2 event listner
+
+// emitr.on("my-age",function(){
+//     console.log("I am 23 year old");
+// })
+// emitr.emit("greet"); //Output Hello greet occured
+// emitr.emit("my-age");
+
+
+emitr.on(events.GREET,function(){
+    console.log("74 love");
+});
+
+emitr.on(events.GREET,function(){
+    console.log("1627 love");
+});
+emitr.on(events.FILEOPEN,function(){
+    console.log("file open of our story!");
+});//added
+
+emitr.emit(events.GREET)
+emitr.emit(events.FILEOPEN)
+
---
+++ l5/emitter.js
@@ -0,0 +1,34 @@
+function Emitter(){
+    this.events = {} //object create krenge
+}
+//prototype se object me new property add kr skte h
+Emitter.prototype.on = function(type,cb){  //nodejs module on use krta hai
+   this.events[type] = this.events[type] || [];
+   this.events[type].push(cb);
+}
+// [cb1,cb2,cb3]-->ise trigger krenge or for each loop lga ke har ek function ko call krenge
+
+/*function Emitter(){
+    this.events = {
+     greet: "namste"
+     } //object create krenge
+}
+//prototype se object me new property add kr skte h
+Emitter.prototype.on = function(type,cb){  //nodejs module on use krta hai
+   this.events["greet"] = this.events[type] //
+}*/ //greet mila to usi greet ko rakhenge nhi to empty array assign kr dneg
+//kuki agr same event honge to usi me add hote jaynge agr same nhi hoga to alg se array me add honge
+
+
+//***** Emit -- event listener ko call krna
+
+Emitter.prototype.emit = function(type){
+    if(this.events[type]){
+        this.events[type].forEach(listner =>{
+         listner();
+        });
+    }
+}
+
+//ab emitter ko export krenge
+module.exports = Emitter;
